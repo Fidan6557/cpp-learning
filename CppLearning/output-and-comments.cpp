c51c8fb86e57fab9bc37d445128d5084991e1739
Fidan6557 <fbbagirova2006@gmail.com>
2026-09-21 01:43:25 +0400

    Add project files.

---
+++ CppLearning/output-and-comments.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main() {
+
+	// This is a comment
+
+	/*
+	This
+	is 
+	multiline 
+	comment
+	*/
+
+	std::cout << "I like pizza!" << '\n';
+	std::cout << "It's really good!" << '\n';
+	return 0;
+}
